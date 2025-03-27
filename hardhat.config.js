dc0c08dc64142e5d6a9c072b035c1b1ddca1e196
Mouloud <94978652+Mouloud007@users.noreply.github.com>
2025-03-27 16:49:20 +0000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,9 +8,9 @@ module.exports = {
         // Define the configuration for the Sepolia test network
         sepolia: {
             // URL of the Sepolia network node provided by Infura
-            url: "https://sepolia.infura.io/v3/08c2c758d9ac43c3bd595071940a9e66",
+            url: "",
             // Array of private keys to use for deploying contracts
-            accounts: ["0xeb65f6f6bde1bbccf4b13389966b73ae409764472da676fe8e9bd14b71d4162c"] // Ensure this is your key
+            accounts: [""] // Ensure this is your key
         }
     }
 };
