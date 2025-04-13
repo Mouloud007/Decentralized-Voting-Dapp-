b3e3cd53afa54b81f2cd614585fbf3c8675e3538
Mouloud <94978652+Mouloud007@users.noreply.github.com>
2025-04-13 17:04:26 +0100

    Update hardhat.config.js
    private key instructions

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
         sepolia: {
 
             url: "https://sepolia.infura.io/v3/08c2c758d9ac43c3bd595071940a9e66",
-            accounts: ["0xeb65f6f6bde1bbccf4b13389966b73ae409764472da676fe8e9bd14b71d4162c"]
+            accounts: ["put your new private key here when you deploy the contract"]
 
         }
     }
