fa40176e3c25db1bad324df22e76b18021944a5f
Mouloud <94978652+Mouloud007@users.noreply.github.com>
2025-04-14 01:24:51 +0100

    Update hardhat.config.js
    Private key instructions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+
 module.exports = {
     solidity: "0.8.0",
     networks: {
         sepolia: {
-
-            url: "https://sepolia.infura.io/v3/08c2c758d9ac43c3bd595071940a9e66",
-            accounts: ["put your new private key here when you deploy the contract"]
-
+            url: "https://sepolia.infura.io/v3/<your-infura-project-id>", // Replace with your Infura Project ID
+            accounts: ["<your-private-key>"] // Replace with your MetaMask private key
         }
     }
 };
